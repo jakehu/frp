e149f06c7e8b731b1ccecb57abbf9bfb5186b27d
jakehu <jakehu1991@gmail.com>
2019-06-27 14:28:50 +0800

    fix.update

--- README.md
+++ README.md
@@ -1,34 +1,34 @@
 ### frps-onekey
 此脚本根据[clangcn/onekey-install-shell](https://github.com/clangcn/onekey-install-shell/tree/master/frps)脚本制作，感谢`Clang`大大
-#### 版本
+##### 版本
 ```
 脚本版本：19.06.27
 
 frps版本：0.27.0
 ```
-#### 安装
+##### 安装
 ```
-wget --no-check-certificate https://raw.githubusercontent.com/jakehu/frp/frps-onekey/master/install-frps.sh -O ./install-frps.sh
+wget --no-check-certificate https://raw.githubusercontent.com/jakehu/frp/master/frps-onekey/install-frps.sh -O ./install-frps.sh
 
 chmod 700 ./install-frps.sh
 
 ./install-frps.sh install
 ```
-#### 卸载
+##### 卸载
 ```
 ./install-frps.sh uninstall
 ```
-#### 更新
+##### 更新
 ```
 ./install-frps.sh update
 ```
-#### 管理
+##### 管理
 ```
 /etc/init.d/frps {start|stop|restart|status|config|version}
 ```
 ### frpc-docker
 
-#### 版本
+##### 版本
 ```
 脚本版本：19.06.27
 
--- frps-onekey/install-frps.sh
+++ frps-onekey/install-frps.sh
@@ -3,8 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
 export FRPS_VER=0.27.0
-export FRPS_INIT="https://raw.githubusercontent.com/jakehu/frp/frps-onekey/master/frps.init"
-export aliyun_download_url="https://code.aliyun.com/jakehu/frp/frps-onekey/raw/master"
+export FRPS_INIT="https://raw.githubusercontent.com/jakehu/frp/master/frps-onekey/frps.init"
+export aliyun_download_url="https://github.com/fatedier/frp/releases/download"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
 #======================================================================
 #   System Required:  CentOS Debian or Ubuntu (32bit/64bit)
@@ -18,7 +18,7 @@ str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
 ver_file="/tmp/.frp_ver.sh"
-str_install_shell="https://raw.githubusercontent.com/jakehu/frp/frps-onekey/master/install-frps.sh"
+str_install_shell="https://raw.githubusercontent.com/jakehu/frp/master/frps-onekey/install-frps.sh"
 shell_update(){
     fun_clangcn "clear"
     echo "Check updates for shell..."
